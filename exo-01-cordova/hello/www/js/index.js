7952918d78ee5255c830e46ce073f4a2ce6a664f
Bachelor 3 <bachelor3@iMac-78.local>
2018-05-03 15:14:59 +0200

    ajout de la date

--- exo-01-cordova/hello/www/js/index.js
+++ exo-01-cordova/hello/www/js/index.js
@@ -41,6 +41,31 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+    
+initialize: function() {
+    document.addEventListener('timestamp', this.onDeviceReady.bind(this), false);
+},
+    
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+
+},
+    
+    // Update DOM on a Received Event
+receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+    
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+    
+    console.log('Received Event: ' + id);
+}
 };
 
 app.initialize();
