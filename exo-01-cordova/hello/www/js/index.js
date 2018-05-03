df7736816c6c8eb0512871fd0153a31c8439e209
Bachelor 3 <bachelor3@iMac-78.local>
2018-05-03 15:20:40 +0200

    ajout date et à la racine aussi

--- exo-01-cordova/hello/www/js/index.js
+++ exo-01-cordova/hello/www/js/index.js
@@ -20,6 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
     },
 
     // deviceready Event Handler
@@ -28,6 +29,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        document.getElementById('timestamp').innerHTML = new Date();
     },
 
     // Update DOM on a Received Event
